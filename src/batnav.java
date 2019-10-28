175a677fbdd8963f52cce15f3fc33093e4f2d7fe
William Chretien <will-189@hotmail.com>
2019-10-27 23:05:58 -0400

    Ajouter des methodes

--- src/batnav.java
+++ src/batnav.java
@@ -11,6 +11,8 @@ public class batnav {
     private static final String MSG_ERR_PLACE = "Erreur! Mauvais placement de bateaux, veuillez recommencer.\n";
     private static final String POS_COL = "  ABCDEFGHIJKLMNOPQR\n";
     private static final char [] TABLEAU_CASES = new char [162];
+    private static int nbBateauxEntre = 0;
+    private static int nbEspaces = 0;
 
     private static void initJeu(){
         for (int i = 0; i < TABLEAU_CASES.length; i++) {
@@ -19,9 +21,10 @@ public class batnav {
         Pep8.stro(MSG_BIENV);
         printTableau();
         Pep8.stro(MSG_ENTRER);
+        verifPlacement();
     }
 
-    private static void printTableau (){
+    private static void printTableau(){
         Pep8.stro(POS_COL);
         for (int i = 0; i < 9; i++) {
             Pep8.stro((i + 1) + "|");
@@ -30,6 +33,97 @@ public class batnav {
             }
             Pep8.stro("|\n");
         }
+    }
+
+    private static void verifPlacement(){
+        char [] descripteurBateaux = entreesPlacements();
+        int i = 0;
+
+        do {
+            if ((nbBateauxEntre == 0 || nbEspaces == nbBateauxEntre) && (verifGrandeur(descripteurBateaux[i]) &&
+                    verifOrientation(descripteurBateaux[i + 1]) && verifColonne(descripteurBateaux[i + 2]) &&
+                    verifRangee(descripteurBateaux[i + 3]))){
+                    nbBateauxEntre++;
+                    i += 4;
+            }else{
+                Pep8.stro(MSG_ERR_PLACE);
+                descripteurBateaux = entreesPlacements();
+            }
+        }while (descripteurBateaux[i] != '\n');
+        for(int j = 0; j < nbBateauxEntre; j++){
+            placementBateau(grandeurEnNb(descripteurBateaux[j * 4]), descripteurBateaux[j * 4 + 1],
+                    colonneEnNb(descripteurBateaux[j * 4 + 2]), rangeeEnNb(descripteurBateaux[j * 4 + 3]));
+        }
+    }
+
+    private static char[] entreesPlacements(){
+        char[] descripteurBateaux = new char[900];
+        char temp;
+        int i = 0;
+        do{
+            temp = Pep8.chari();
+            if(temp != ' ') {
+                descripteurBateaux[i] = temp;
+                i++;
+            }else{
+                nbEspaces++;
+            }
+        }while (temp != '\n');
+        return descripteurBateaux;
+    }
+
+    private static boolean verifGrandeur(char grandeur){
+        return grandeur == 'p' || grandeur == 'm' || grandeur == 'g';
+    }
+
+    private static boolean verifOrientation(char orientation){
+        return orientation == 'h' || orientation == 'v';
+    }
+
+    private static boolean verifColonne(char colonne){
+        return colonne >= 'A' && colonne <= 'R';
+    }
+
+    private static boolean verifRangee(char rangee) {
+        return rangee >= '1' && rangee <= '9';
+    }
+
+    private static int colonneEnNb(char colonne){
+        return colonne - 'A';
+    }
+
+    private static int rangeeEnNb(char rangee){
+        return rangee - '1';
+    }
+
+    private static int grandeurEnNb(char grandeur){
+        int nbCases;
+        switch (grandeur){
+            case 'p': nbCases =1;
+                break;
+            case 'm': nbCases = 3;
+                break;
+            default: nbCases = 5;
+                break;
+        }
+        return nbCases;
+    }
+
+    //a continuer
+    private static void placementBateau(int grandeur, char orientation, int colonne, int rangee) {
+        char orientationChar = 'v';
+        if(orientation == 'h'){
+            orientationChar = '>';
+        }
+
+        for (int i = 0; i < grandeur; i++) {
+            if(orientation == 'h') {
+                TABLEAU_CASES[colonne + (rangee * 18) + i] = orientationChar;
+            }else{
+                TABLEAU_CASES[colonne + (rangee * 18) + i * 18] = orientationChar;
+            }
+        }
+        printTableau();
     }
 
     public static void main (String [] args){
